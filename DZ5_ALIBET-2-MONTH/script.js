f17359219972752950fafd6e4785468fa9ffe2f9
Diyes-Alibet <alibetasanov7@gmail.com>
2024-09-19 20:36:29 +0600

    Add files via upload

---
+++ DZ5_ALIBET-2-MONTH/script.js
@@ -0,0 +1,46 @@
+const input = document.getElementById("input");
+const createButton = document.getElementById("create_button");
+const todoList = document.getElementById("todo_list");
+
+const createTodo = () => {
+
+    if (input.value.trim() === "") return alert("Напишите что-нибудь!");
+    const div = document.createElement("div");
+    const text = document.createElement('h3');
+    const buttons_div = document.createElement("div");
+    const editButton = document.createElement("button");
+    const deleteButton = document.createElement("button");
+
+    div.setAttribute('class', 'block_text');
+    buttons_div.setAttribute("class", "buttons_div");
+    deleteButton.setAttribute('class', 'delete_button');
+    editButton.setAttribute('class', 'edit_button');
+    text.innerHTML = input.value;
+
+
+
+    input.value = "";
+    editButton.textContent = "EDIT";
+    editButton.onclick = () => {
+        const newTodoText = prompt('Введите новый текст', text.textContent);
+        if (newTodoText) {
+            text.textContent = newTodoText;
+        }
+    }
+
+    deleteButton.textContent = "DELETE";
+    deleteButton.onclick = () => div.remove();
+
+
+    buttons_div.append(deleteButton);
+    buttons_div.append(editButton);
+                      div.append(text);
+             todoList.append(div);
+      div.append(buttons_div);
+};
+
+createButton.onclick = () => createTodo();
+
+    input.addEventListener("keydown", (e) => {
+                    if (e.key === 'Enter') return createTodo(); {}
+});
